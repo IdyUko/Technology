ca176ae9304b7c61fc89a91724f30879a8897e9d
IdyUko <idyuko@gmail.com>
2023-05-10 18:13:26 +0100

    Technology

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "./component/Home/home";
+import styled from "styled-components";
+
+
+const App= () => {
+  return (
+        <Div>
+                <Home/>
+        </Div>
+      
+  )
+};
+
+export default App;
+
+const Div = styled.div`
+`;
